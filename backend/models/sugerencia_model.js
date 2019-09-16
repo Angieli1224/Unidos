7257902f16f23b35c987996ef15a6cf41cabdcf4
Angie Lizeth Hurtado <angieli1224@gmail.com>
2019-09-15 21:11:05 -0500

    validacion

--- backend/models/sugerencia_model.js
+++ backend/models/sugerencia_model.js
@@ -6,6 +6,8 @@ let SugerenciaSchema = new Schema({
 
    // id_usuario: { type: Schema.Types.ObjectId, ref: 'Usuario', required: [true, 'Se requiere el "id_usuario" para crear el registro.'] },
    id_usuario: { type: Number, required: [true, 'Se requiere el "id_usuario" para crear el registro.'] },
+   nombre: { type: String, required: [true, 'Se requiere el "id_usuario" para crear el registro.'] },
+   telefono: { type: Number, required: [true, 'Se requiere el "id_usuario" para crear el registro.'] },
    id_enlace: { type: Number, required: [true, 'Se requiere el "id_enlace" para crear el registro.'] },
    asunto: { type: String, required: [true, 'Se requiere el "asunto" para crear el registro.'] },
    descripcion: { type: String, required: [true, 'Se requiere el "descripcion" para crear el registro.'] },
